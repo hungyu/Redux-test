6d675f06d0e1a1a5f99cb2233910ff617f517793
Josh <nowonderg@gmail.com>
2020-09-08 15:01:57 +0800

    posts feed with redux

---
+++ src/features/posts/AddPostForm.js
@@ -0,0 +1,50 @@
+import React, { useState } from 'react'
+import { useDispatch } from 'react-redux'
+import { nanoid } from '@reduxjs/toolkit'
+
+import { postAdded } from './postsSlice';
+
+const AddPostForm = props => {
+	const [title, setTitle] = useState('');
+	const [content, setContent] = useState('');
+
+	const dispatch = useDispatch();
+
+	const onTitleChanged = e => setTitle(e.target.value);
+	const onContentChanged = e => setContent(e.target.value);
+	const onSaveButtonClicked = e => {
+
+		if (title && content) {
+			dispatch(
+				postAdded({
+          id: nanoid(),
+          title,
+          content
+				})
+			)
+		}
+	}
+
+
+	return (
+		<section>
+			<h2>Add new Posts</h2>
+			<form action="">
+				<label htmlFor=""></label>
+				<input
+					type="text"
+					id="post-title"
+					value={title}
+					onChange={onTitleChanged}
+				/>
+				<label htmlFor=""></label>
+				<textarea name="post-content" id="post-content" value={content} onChange={onContentChanged}></textarea>
+				<button type="button" onClick={onSaveButtonClicked}>
+					Save Posts
+				</button>
+			</form>
+		</section>
+	)
+}
+
+export default AddPostForm
