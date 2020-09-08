017d3a61732d29ba07e1add015a931151df0ca17
Josh <nowonderg@gmail.com>
2020-09-08 21:15:09 +0800

    add reaction button on single post page

--- src/features/posts/SinglePostPage.js
+++ src/features/posts/SinglePostPage.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { useSelector } from 'react-redux'
 import { Link } from 'react-router-dom'
 import PostAuthor from './PostAuthor'
+import ReactionButtons from './ReactionButtons';
 
 const SinglePostPage = ({ match }) => {
 	const { postId } = match.params
@@ -22,6 +23,7 @@ const SinglePostPage = ({ match }) => {
         <h2>{post.title}</h2>
         <p className="post-content">{post.content}</p>
         <PostAuthor userId={post.user} />
+        <ReactionButtons post={post} />
 				<Link to={`/editPost/${post.id}`} className="button">
           Edit Post
         </Link>
